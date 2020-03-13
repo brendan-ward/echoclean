4ff6249fd94256bbf4a7b227c369d075a7571ebc
Brendan C. Ward <bcward@astutespruce.com>
2020-03-13 16:11:11 -0700

    Initial release on pypi

--- setup.py
+++ setup.py
@@ -1,37 +1,23 @@
 import os
-from setuptools import setup, find_packages
-
-
-long_description = 'Utility to help apply expert rules to sonobat output'
-
-
-if os.path.exists('README.md'):
-    try:
-        # Use pypandoc to convert markdown readme to reStructuredText as required by pypi
-        # Requires pandoc to be installed.  See: http://johnmacfarlane.net/pandoc/installing.html
-        from pypandoc import convert
-        read_md = lambda f: convert(f, 'rst', format='md')
-        long_description = read_md('README.md')
-    except:
-        pass
+from setuptools import setup
 
 setup(
     name='echoclean',
     version='0.1.0',
-    description=u"Utility to help apply expert rules to sonobat output",
-    long_description=long_description,
-    classifiers=[],
+    packages=['echoclean'],
+    url='https://github.com/brendan-ward/echoclean',
     keywords='sonobat, echolocation monitoring',
-    author=u"Brendan Ward",
-    author_email='bcward@consbio.org',
-    url='https://github.com/consbio/echoclean',
-    license='ISC',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    license='MIT',
+    author='Brendan C. Ward',
+    author_email='bcward@astutespruce.com',
+    description='Utility to help apply expert rules to sonobat output',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
       'click',
-      'openpyxl',
+      'openpyxl==2.5.14',
       'six'
     ],
     extras_require={
